ef7f622ceca4109a216f17effeceb70a9f05349e
YuryGe <postbox177@yandex.ru>
2024-09-09 15:13:26 +0400

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,29 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    public static void main(String[] args) {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        progress.interrupt();
+    }
+
+    @Override
+    public void run() {
+        var process = new char[] {'-', '\\', '|', '/'};
+        var index = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                break;
+            }
+            System.out.printf("\r load: %c", process[index]);
+            index = index == process.length - 1 ? 0 : index + 1;
+        }
+    }
+}
