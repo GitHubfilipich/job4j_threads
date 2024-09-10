babd6a00d99bda564ba8c26e8e689e4bce6443fd
YuryGe <postbox177@yandex.ru>
2024-09-10 08:53:29 +0400

    7. Приоритеты нитей и нити-демоны [#504907] пример

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -11,13 +11,14 @@ public class ThreadState {
         System.out.printf("first: %s%n", first.getState());
         System.out.printf("second: %s%n", second.getState());
         first.start();
+        second.setDaemon(true);
         second.start();
         while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
             System.out.printf("first: %s%n", first.getState());
             System.out.printf("second: %s%n", second.getState());
         }
         System.out.println("The job is done!");
-        System.out.printf("first: %s%n", first.getState());
-        System.out.printf("second: %s%n", second.getState());
+        System.out.printf("first: %s isdemon %s%n", first.getState(), first.isDaemon());
+        System.out.printf("second: %s isdemon %s%n", second.getState(), second.isDaemon());
     }
 }
