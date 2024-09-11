8602226be9d65ac512661a3eec94b26426590eb3
YuryGe <postbox177@yandex.ru>
2024-09-11 10:44:57 +0400

    8. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -1,5 +1,8 @@
 package ru.job4j.thread;
 
+import java.io.IOException;
+import java.net.URL;
+
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
@@ -11,14 +14,43 @@ public class Wget implements Runnable {
 
     @Override
     public void run() {
-        /* Скачать файл*/
+        try (var input = new URL(url).openStream()) {
+            var dataBuffer = new byte[512];
+            var downloadAt = System.nanoTime();
+            while (input.read(dataBuffer, 0, dataBuffer.length) != -1) {
+                var realSpeed = 512 * 1000000 / (System.nanoTime() - downloadAt);
+                if (realSpeed > speed) {
+                    try {
+                        Thread.sleep(realSpeed / speed);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+                downloadAt = System.nanoTime();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) throws InterruptedException {
+        validate(args);
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
         Thread wget = new Thread(new Wget(url, speed));
         wget.start();
         wget.join();
+    }
+
+    private static void validate(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Not enough parameters");
+        }
+        if (args[0].isBlank()) {
+            throw new IllegalArgumentException("Incorrect first parameter");
+        }
+        if (args[1].isBlank() || !args[1].matches("\\d+") || Integer.parseInt(args[1]) == 0) {
+            throw new IllegalArgumentException("Incorrect second parameter");
+        }
     }
 }
