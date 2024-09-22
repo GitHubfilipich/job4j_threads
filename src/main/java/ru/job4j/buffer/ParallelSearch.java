1996324ec544a99c0ed4d4268de205449fef66d3
YuryGe <postbox177@yandex.ru>
2024-09-22 12:08:41 +0400

    2. Обеспечить остановку потребителя. [#66825] пример

---
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,41 @@
+package ru.job4j.buffer;
+
+import ru.job4j.SimpleBlockingQueue;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(3);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (true) {
+                        try {
+                            System.out.println("poll " + queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                            System.out.println("offer " + index);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+        ).start();
+    }
+}
