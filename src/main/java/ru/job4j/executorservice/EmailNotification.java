e58cb1170bb5bcefcc825380cd68d74eabbd64ad
YuryGe <postbox177@yandex.ru>
2024-09-29 12:59:20 +0400

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/executorservice/EmailNotification.java
@@ -0,0 +1,21 @@
+package ru.job4j.executorservice;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+    private final ExecutorService pool = Executors.newCachedThreadPool();
+
+    public void emailTo(User user) {
+        String subject = String.format("Notification {%s} to email {%s}", user.username(), user.email());
+        String body = String.format("Add a new event to {%s}", user.username());
+        pool.submit(() -> send(subject, body, user.email()));
+    }
+
+    public void close() {
+        pool.close();
+    }
+
+    public void send(String subject, String body, String email) {
+    }
+}
