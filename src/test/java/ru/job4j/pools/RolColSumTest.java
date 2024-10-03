773f0b76c1d9864474d9e47561a4ddfa3d29531b
YuryGe <postbox177@yandex.ru>
2024-10-03 08:53:13 +0400

    4. CompletableFuture [#361626 #515666] исправление 1

--- src/test/java/ru/job4j/pools/RolColSumTest.java
+++ src/test/java/ru/job4j/pools/RolColSumTest.java
@@ -10,18 +10,18 @@ class RolColSumTest {
     @Test
     public void whenSumMatrix3() {
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        RolColSum.Sums[] expected = {new RolColSum.Sums(6, 12),
-                new RolColSum.Sums(15, 15),
-                new RolColSum.Sums(24, 18)};
+        Sums[] expected = {new Sums(6, 12),
+                new Sums(15, 15),
+                new Sums(24, 18)};
         assertThat(RolColSum.sum(matrix)).containsExactly(expected);
     }
 
     @Test
     public void whenAsyncSumMatrix3() throws ExecutionException, InterruptedException {
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        RolColSum.Sums[] expected = {new RolColSum.Sums(6, 12),
-                new RolColSum.Sums(15, 15),
-                new RolColSum.Sums(24, 18)};
+        Sums[] expected = {new Sums(6, 12),
+                new Sums(15, 15),
+                new Sums(24, 18)};
         assertThat(RolColSum.asyncSum(matrix)).containsExactly(expected);
     }
 
